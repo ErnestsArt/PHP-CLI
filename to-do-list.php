3d746ee634c302891e35e91d6171b39a613ab122
Ernests <ernests1012@gmail.com>
2024-09-17 21:05:15 +0300

    updating to-do-list file so it works

--- to-do-list.php
+++ to-do-list.php
@@ -2,50 +2,41 @@
 
 $taskList = ["first task", "second task"];
 
+
 function showAllTask($inputTasks) {
     foreach($inputTasks as $task){
         echo $task . "\n";
     }
-
 }
 
-// $name = readline("ievadi savu vārdu: \n");
 
-// echo "your name is $name";
-
-// 1.uzdevums
-// uztaisīt do-while ciklu
-// var apstādināt ievadot : N
-
-//2.uzdevums
-//izveidot switch-case konstrukciju
-// lai apstrādātu dažādas lietotāja ievades
+function addNewTask(&$taskList) {
+    $newTask = readline("Ievadiet jaunu uzdevumu: ");
+    $taskList[] = $newTask; 
+    echo "Uzdevums pievienots!\n";
+}
 
 do {
     echo "uzdevumu pārvaldnieks\n";
-    echo "ievadīt jaunu uzdevumu => 1\n";
-    echo "apskatīt uzdevumu => 2\n";
+    echo "apskatīt uzdevumus => 1\n";
+    echo "ievadīt jaunu uzdevumu => 2\n";
     echo "apskatīt uzdevumu => 3\n";
     $choice = readline("izvēlies darbību: ");
 
-    $favcolor = 'red';
-
     switch ($choice) {
       case '1':
-        showTask($taskList);
+        showAllTask($taskList);
         break;
       case '2':
-        echo "To be implemented";
+        addNewTask($taskList);
         break;
       case '3':
-        echo "To be implemented";
+        echo "To be implemented\n";
         break;
       default:
         echo "invalid option!!!!!!!\n";
     }
-    
 
-    echo "turpinam?: $x \n";
-    $continue = readline("vai vēlies turpināt\n");
-  } while ($continue != "N");
-  
+    $continue = readline("vai vēlies turpināt (N lai pārtrauktu)? ");
+} while (strtoupper($continue) != "N");
+
